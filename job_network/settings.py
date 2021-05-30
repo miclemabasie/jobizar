275eb706cc08e549447665b074cc72d368849d99
MICLEM <miclemabasie3@gmail.com>
2021-05-30 23:20:12 +0100

    Changed env variables

--- job_network/settings.py
+++ job_network/settings.py
@@ -29,10 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+
+SECRET_KEY="712d880b903580e2a3aef42401d04d7ad769ea7a70988132"
+DEBUG=True
 
 ALLOWED_HOSTS = ["jobizar.herokuapp.com", "localhost"]
 
