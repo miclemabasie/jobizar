53995d5a77a40c964f0e565f49f207089b6586ce
MICLEM <miclemabasie3@gmail.com>
2021-05-30 05:17:50 +0100

    modified the database settings

--- job_network/settings.py
+++ job_network/settings.py
@@ -139,16 +139,24 @@ WSGI_APPLICATION = 'job_network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "job_lister",
+#         'HOST': 'localhost',
+#         'PORT': "5432",
+#         'USER': "miclem",
+#         'PASSWORD': "12electron@3#"
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "job_lister",
-        'HOST': 'localhost',
-        'PORT': "5432",
-        'USER': "miclem",
-        'PASSWORD': "12electron@3#"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Password validation
