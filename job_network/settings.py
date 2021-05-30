3e5e9ad68222911bdbc1363a5375d46285d53007
MICLEM <miclemabasie3@gmail.com>
2021-05-30 23:14:41 +0100

    modified enviroment variables

--- job_network/settings.py
+++ job_network/settings.py
@@ -20,6 +20,7 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["jobizar.herokuapp.com", "localhost"]
 
